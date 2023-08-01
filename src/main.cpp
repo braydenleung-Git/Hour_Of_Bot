48ab6644e1eec3b1f60408be29f779bd178fc82b
braydenleung-Git <brayden.leung@gmail.com>
2023-08-01 21:32:41 +0800

    slight modifications the json's and added a reply message to shutdown the robot

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,39 @@
 #include "config_s.h"
 #include <dpp/dpp.h>
 const std::string    BOT_TOKEN    = token;
-
 int main() {
-    dpp::cluster bot(BOT_TOKEN);   
+    dpp::cluster bot(BOT_TOKEN); 
 
     bot.on_log(dpp::utility::cout_logger());
 
     bot.on_slashcommand([](const dpp::slashcommand_t& event) {
         if (event.command.get_command_name() == "ping") {
-            event.reply("Pong! connection Valid!");
+            event.reply("Pong! Why did you typed this");
         }
     });
 
     bot.on_slashcommand([&bot](const dpp::slashcommand_t& event) {
         if (event.command.get_command_name() == "stop") {
-            bot.shutdown();
+            event.reply("Turning off Bot now");
         }
+        std::cout<<"robot has been turned off"<<endl;
+        bot.shutdown();
+        std::cout<<"the program has exited"<<endl;
+        return 0;
     });
 
-    bot.on_ready([&bot](const dpp::ready_t& event) {
+    bot.on_ready([&bot](const dpp::ready_t & event) {
+        std:: cout <<" Hour Of Bot project has been turned on"<<endl;
         if (dpp::run_once<struct register_bot_commands>()) {
-            bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
-            bot.global_command_create(dpp::slashcommand("stop", "Turns off the bot", bot.me.id));
-        }
-    }); 
-    
+            dpp::slashcommand ping("ping","I wonder what this do?", bot.me.id);
+            dpp::slashcommand stop ("stop", "turns the robot down", bot.me.id);
+            stop.set_dm_permission(true);
+            stop.set_default_permissions(0);
+            bot.global_command_create(ping);
+            bot.global_command_create(stop);
 
+        }
+    });
     bot.start(dpp::st_wait);
+
 }
