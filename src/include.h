857cc765fb8d558192fd2f94996965ef5c3c8df7
braydenleung-Git <brayden.leung@gmail.com>
2023-10-31 19:57:12 -0400

    updated include.h

--- src/include.h
+++ src/include.h
@@ -1,10 +1,10 @@
-#include <spdlog/spdlog.h>
-#include <spdlog/async.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/sinks/rotating_file_sink.h>
+//#include <spdlog/spdlog.h>
+//#include <spdlog/async.h>
+//#include <spdlog/sinks/stdout_color_sinks.h>
+//#include <spdlog/sinks/rotating_file_sink.h>
 #include <iomanip>
 #include <dpp/dpp.h>
-#include <fmt/format.h>
+//#include <fmt/format.h>
 #include <fstream>
 #include <ctime>
 
