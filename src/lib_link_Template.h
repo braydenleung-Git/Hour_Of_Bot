a4f94215bfa61d5410eaa3877457621a092065cc
braydenleung-Git <brayden.leung@gmail.com>
2023-11-01 20:09:20 -0400

    modified:   src/lib_link_Template.h

--- src/lib_link_Template.h
+++ src/lib_link_Template.h
@@ -1,2 +1,2 @@
 //This is a template for linking library that is installed locally on device, duplicate and remove the "_Template", 
-//and paste any library that is downloaded on system
+//and paste any library that is downloaded on system, Do not include any library again if you have previously installed the library on system
